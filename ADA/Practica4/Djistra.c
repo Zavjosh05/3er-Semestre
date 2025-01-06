d09bca2cbdff540ae49a6e58b5f45cf18a103130
Omar Jesús Vázquez Sánchez <omar_jv.s@hotmail.com>
2025-01-05 19:40:51 -0600

    Fin de Djistra
    XD

--- ADA/Practica4/Djistra.c
+++ ADA/Practica4/Djistra.c
@@ -4,13 +4,17 @@
 #include"Nodo.h"
 
 void algoritmo(Nodo * listaNodos, Arista * listaAristas, int tamListaNodos, int tamListaAristas);
-int comprobarNodoExiste(Nodo * listaNodos, char nodoABuscar, int tamListaNodos);
+int comprobarNodoExiste(Nodo * listaNodos, char nodoABuscar, int tamListaNodos, int op);
 int mostrarPosicionEnArray(Nodo * listaNodos, char nodoABuscar, int tamListaNodos);
-int rutaPorPesoPequeno(Nodo node, char nodoFinal, Nodo * listaNodosRecorridos, int * tamListaNodosRecorridos);
+int rutaPorPesoPequeno(Nodo node, char nodoFinal, Nodo ** listaNodosRecorridos, int * tamListaNodosRecorridos);
 void acomodarAristasDeNodos(Nodo * listaNodos, int tamListaNodos);
 void acomodarAristasDeNodo(Nodo node);
-Nodo * agregarNodoAListaNodosRecorrido(Nodo * listaNodosRecorrido, Nodo nodoAAgregar, int * tamListaNodosRecorrido);
+Nodo ** agregarNodoAListaNodosRecorrido(Nodo ** listaNodosRecorrido, Nodo nodoAAgregar, int * tamListaNodosRecorrido);
 Nodo nodoTarea();
+Nodo ** quitarUltimoNodoAListaNodosRecorrido(Nodo ** listaNodosRecorrido, int * tamListaNodosRecorrido);
+int rutaPorPesoPequenoExhaustivo(Nodo node, char nodoInial, char nodoFinal, Nodo ** listaNodosRecorridos, int * tamListaNodosRecorridos, Nodo ** resultadoFinalListaNodosRecorridos, int * pesoFinal, int * tamresultadoFinalListaNodosRecorridos);
+void crearCopiaDeLaListaNodosRecorridos(Nodo ** listaNodosRecorridos, Nodo ** resultadoFinalListaNodosRecorridos, int tamListaNodosRecorridos);
+int pesoDelResultado(Nodo * listaNodosRecorridos, int tamListaNodosRecorridos);
 
 int main(void)
 {
@@ -87,47 +91,81 @@ Nodo nodoTarea()
 void algoritmo(Nodo * listaNodos, Arista * listaAristas, int tamListaNodos, int tamListaAristas)
 {
   char nodoInicial, nodoFinal;
-  int tamListaNodosRecorridos = 1;
+  int tamListaNodosRecorridos = 0, tamListaNodosRecorridos2 = 0, pesoFinal = 0, tamsolucionListaNodosRecorridos = 0;
   do
   {
     printf("Ingresa el nodo incial\n");
     scanf("%c", &nodoInicial);
     getchar();
-  }while(!comprobarNodoExiste(listaNodos, nodoInicial, tamListaNodos));
+  }while(!comprobarNodoExiste(listaNodos, nodoInicial, tamListaNodos, 1));
   do
   {
     printf("Ingresa el nodo final\n");
     scanf("%c", &nodoFinal);
     getchar();
-  }while(!comprobarNodoExiste(listaNodos, nodoFinal, tamListaNodos));
+  }while(!comprobarNodoExiste(listaNodos, nodoFinal, tamListaNodos, 1));
+
   int posicionNodoInicial = mostrarPosicionEnArray(listaNodos, nodoInicial, tamListaNodos);
+
   acomodarAristasDeNodos(listaNodos, tamListaNodos);
+
   imprimirArregloDeNodos(listaNodos, tamListaNodos);
+
   Nodo * listaNodosRecorridos = (Nodo *) malloc(sizeof(Nodo));
+  Nodo * listaNodosRecorridos2 = (Nodo *) malloc(sizeof(Nodo));
+  Nodo * solucionListaNodosRecorridos = (Nodo *) malloc(sizeof(Nodo));
+
   if(listaNodosRecorridos == NULL)
     exit(-1);
-  listaNodosRecorridos[0] = listaNodos[posicionNodoInicial];
-  rutaPorPesoPequeno(listaNodos[posicionNodoInicial], nodoFinal, listaNodosRecorridos, &tamListaNodosRecorridos);
+
+  rutaPorPesoPequeno(listaNodos[posicionNodoInicial], nodoFinal, &listaNodosRecorridos, &tamListaNodosRecorridos);
+
+  printf("\nResultado:\n");
+  imprimirArregloDeNodos2(listaNodosRecorridos, tamListaNodosRecorridos);
+
+  printf("Inicia proceso exhaustivo");
+  rutaPorPesoPequenoExhaustivo(listaNodos[posicionNodoInicial], nodoInicial, nodoFinal, &listaNodosRecorridos2, &tamListaNodosRecorridos2, &solucionListaNodosRecorridos, &pesoFinal, &tamsolucionListaNodosRecorridos);
+  printf("\nResultado exhaustivo:\n");
+  imprimirArregloDeNodos2(solucionListaNodosRecorridos, tamsolucionListaNodosRecorridos);
 }
 
-int comprobarNodoExiste(Nodo * listaNodos, char nodoABuscar, int tamListaNodos)
+int comprobarNodoExiste(Nodo * listaNodos, char nodoABuscar, int tamListaNodos, int op)
 {
-  printf("\ntamListaNodos %d\n", tamListaNodos);
   for(int i = 0; i < tamListaNodos; i++)
   {
-    printf("\nHola mundo\n");
-    printf("\n%c y %c\n", listaNodos[i]->nombre, nodoABuscar);
     if(listaNodos[i]->nombre == nodoABuscar)
       return 1;
   }
-  printf("El nodo no existe\n");
+  if(op)
+    printf("El nodo no existe\n");
   return 0;
 }
 
-int rutaPorPesoPequeno(Nodo node, char nodoFinal, Nodo * listaNodosRecorridos, int * tamListaNodosRecorridos)
+int rutaPorPesoPequeno(Nodo node, char nodoFinal, Nodo ** listaNodosRecorridos, int * tamListaNodosRecorridos)
 {
+  listaNodosRecorridos = agregarNodoAListaNodosRecorrido(listaNodosRecorridos, node, tamListaNodosRecorridos);
 
+  Nodo nodeSiguiente = NULL;
+
+  if(node->nombre == nodoFinal)
+    return 1;
+
+  for(int i = 0; i < node->numAristas; i++)
+  {
+
+    if(node == node->aristas[i]->n1)
+      nodeSiguiente = node->aristas[i]->n2;
+    else
+      nodeSiguiente = node->aristas[i]->n1;
+    if(comprobarNodoExiste(*listaNodosRecorridos, nodeSiguiente->nombre, *tamListaNodosRecorridos, 0))
+      continue;
+    if(rutaPorPesoPequeno(nodeSiguiente, nodoFinal, listaNodosRecorridos, tamListaNodosRecorridos))
+      return 1;
+  }
+  listaNodosRecorridos = quitarUltimoNodoAListaNodosRecorrido(listaNodosRecorridos, tamListaNodosRecorridos);
+  return 0;
 }
+
 
 
 int mostrarPosicionEnArray(Nodo * listaNodos, char nodoABuscar, int tamListaNodos)
@@ -163,12 +201,81 @@ void acomodarAristasDeNodo(Nodo node)
   }
 }
 
-Nodo * agregarNodoAListaNodosRecorrido(Nodo * listaNodosRecorrido, Nodo nodoAAgregar, int * tamListaNodosRecorrido)
+Nodo ** agregarNodoAListaNodosRecorrido(Nodo ** listaNodosRecorrido, Nodo nodoAAgregar, int * tamListaNodosRecorrido)
 {
-  *tamListaNodosRecorrido++;
-  listaNodosRecorrido = (Nodo *) realloc(listaNodosRecorrido, sizeof(Nodo) * (*tamListaNodosRecorrido));
-  if(listaNodosRecorrido == NULL)
+  (*tamListaNodosRecorrido)++;
+  (*listaNodosRecorrido) = (Nodo *) realloc((*listaNodosRecorrido), sizeof(Nodo) * (*tamListaNodosRecorrido));
+  if((*listaNodosRecorrido) == NULL)
     exit(-1);
-  listaNodosRecorrido[*tamListaNodosRecorrido - 1] = nodoAAgregar;
+  (*listaNodosRecorrido)[(*tamListaNodosRecorrido) - 1] = nodoAAgregar;
   return listaNodosRecorrido;
+}
+
+Nodo ** quitarUltimoNodoAListaNodosRecorrido(Nodo ** listaNodosRecorrido, int * tamListaNodosRecorrido)
+{
+  (*listaNodosRecorrido)[(*tamListaNodosRecorrido)-1] = NULL;
+  (*tamListaNodosRecorrido)--;
+  (*listaNodosRecorrido) = (Nodo *) realloc((*listaNodosRecorrido), sizeof(Nodo) * (*tamListaNodosRecorrido));
+  if((*listaNodosRecorrido) == NULL)
+    exit(-1);
+  return listaNodosRecorrido;
+}
+
+int rutaPorPesoPequenoExhaustivo(Nodo node, char nodoInial, char nodoFinal, Nodo ** listaNodosRecorridos, int * tamListaNodosRecorridos, Nodo ** resultadoFinalListaNodosRecorridos, int * pesoFinal, int * tamresultadoFinalListaNodosRecorridos) {
+  listaNodosRecorridos = agregarNodoAListaNodosRecorrido(listaNodosRecorridos, node, tamListaNodosRecorridos);
+
+  Nodo nodeSiguiente = NULL;
+
+  if(node->nombre == nodoFinal) {
+    int peso = pesoDelResultado(*listaNodosRecorridos, *tamListaNodosRecorridos);
+    if(peso < *pesoFinal || *pesoFinal == 0)
+    {
+      crearCopiaDeLaListaNodosRecorridos(listaNodosRecorridos, resultadoFinalListaNodosRecorridos, *tamListaNodosRecorridos);
+      (*pesoFinal) = peso;
+      *tamresultadoFinalListaNodosRecorridos = *tamListaNodosRecorridos;
+    }
+    listaNodosRecorridos = quitarUltimoNodoAListaNodosRecorrido(listaNodosRecorridos, tamListaNodosRecorridos);
+    return 1;
+  }
+
+  for(int i = 0; i < node->numAristas; i++)
+  {
+    if(node == node->aristas[i]->n1)
+      nodeSiguiente = node->aristas[i]->n2;
+    else
+      nodeSiguiente = node->aristas[i]->n1;
+    if(comprobarNodoExiste(*listaNodosRecorridos, nodeSiguiente->nombre, *tamListaNodosRecorridos, 0))
+      continue;
+    rutaPorPesoPequenoExhaustivo(nodeSiguiente, nodoInial, nodoFinal, listaNodosRecorridos, tamListaNodosRecorridos, resultadoFinalListaNodosRecorridos, pesoFinal, tamresultadoFinalListaNodosRecorridos);
+  }
+  if(node->nombre != nodoInial)
+    listaNodosRecorridos = quitarUltimoNodoAListaNodosRecorrido(listaNodosRecorridos, tamListaNodosRecorridos);
+  return 0;
+}
+
+void crearCopiaDeLaListaNodosRecorridos(Nodo ** listaNodosRecorridos, Nodo ** resultadoFinalListaNodosRecorridos, int tamListaNodosRecorridos)
+{
+  (*resultadoFinalListaNodosRecorridos) = NULL;
+  (*resultadoFinalListaNodosRecorridos) = (Nodo*) malloc(tamListaNodosRecorridos * sizeof(Nodo));
+  if((*resultadoFinalListaNodosRecorridos) == NULL)
+    exit(-1);
+  for (int i = 0; i < tamListaNodosRecorridos; i++)
+    (*resultadoFinalListaNodosRecorridos)[i] = (*listaNodosRecorridos)[i];
+}
+
+int pesoDelResultado(Nodo * listaNodosRecorridos, int tamListaNodosRecorridos)
+{
+  int peso = 0;
+  for(int i = 0; i < tamListaNodosRecorridos - 1; i++)
+  {
+    for(int j = 0; j < listaNodosRecorridos[i]->numAristas; j++)
+    {
+      if(listaNodosRecorridos[i+1] == listaNodosRecorridos[i]->aristas[j]->n1 || listaNodosRecorridos[i+1] == listaNodosRecorridos[i]->aristas[j]->n2)
+      {
+        peso += listaNodosRecorridos[i]->aristas[j]->valor;
+        break;
+      }
+    }
+  }
+  return peso;
 }
