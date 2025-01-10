ce9516dd772c3ed7c442448af21869283e61ef10
Zavaleta Guerrero Joshua Ivan <118492689+Zavjosh05@users.noreply.github.com>
2025-01-09 23:25:28 -0600

    ola

---
+++ ADA/HUFFMAN/Prueba.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(int argc, char **argv)
+{
+
+}
+
+
+
+int numeroDeElementosDeArchivo(char *archivo)
+{
+
+}
+
+char *obtenerArchivo(char *archivo)
+{
+    FILE *arch;
+    int i = 0;
+    char *arr;
+
+
+}
