aac95a9a8f1c8cb16157d3a47069387f841d736e
Omar Jesús Vázquez Sánchez <omar_jv.s@hotmail.com>
2025-01-11 17:36:50 -0600

    XD, ya quedo
    LOL

--- ADA/HUFFMAN/TruchaCompresor.c
+++ ADA/HUFFMAN/TruchaCompresor.c
@@ -2,7 +2,7 @@
 #include <commdlg.h>
 #include <stdio.h>
 #include <string.h>
-int obtenerRuta(char ** rutaSinNombreArchivo, char ** nombreArchivo);
+int obtenerRuta(char ** rutaSinNombreArchivo, char ** nombreArchivo, char ** extension);
 
 /*
  * Debemos tener los siguientes argumentos -lgdi32 -lcomdlg32
@@ -11,12 +11,11 @@ int obtenerRuta(char ** rutaSinNombreArchivo, char ** nombreArchivo);
 
 int main()
 {
-	char * rutaSinNombreArchivo;
-	char * nombreArchivo;
-
-	obtenerRuta(&rutaSinNombreArchivo, &nombreArchivo);
-
-	printf("La ruta es: %s%s\n", rutaSinNombreArchivo, nombreArchivo);
+  char * rutaSinNombreArchivo;
+  char * nombreArchivo;
+  char * extension;
+  obtenerRuta(&rutaSinNombreArchivo, &nombreArchivo, &extension);
+  printf("La ruta es: %s%s%s\n", rutaSinNombreArchivo, nombreArchivo, extension);
 }
 
 /**
@@ -24,38 +23,30 @@ int main()
  *
  * @param rutaSinNombreArchivo Devuelve la ruta sin el nombre del archivo
  * @param nombreArchivo Devuelve el nombre del archivo
+ * @param
  */
-int obtenerRuta(char ** rutaSinNombreArchivo, char ** nombreArchivo)
+int obtenerRuta(char ** rutaSinNombreArchivo, char ** nombreArchivo, char ** extension)
 {
-
-	// Estructura OPENFILENAME para configurar el diálogo
-	OPENFILENAME ofn;
-	char szFile[260] = {0};
-	char szFileTitle[260] = {0};
-
-	// Inicializar la estructura OPENFILENAME
-	ZeroMemory(&ofn, sizeof(ofn));
-	ofn.lStructSize = sizeof(ofn);
-	ofn.hwndOwner = NULL; // Ventana propietaria (NULL para consola)
-
-	ofn.lpstrFile = szFile; // Buffer para el archivo seleccionado
-	ofn.nMaxFile = sizeof(szFile); // Tamaño del buffer
-
-	ofn.lpstrFileTitle = szFileTitle; //Guarda el nombre ya sea con la ruta completa o solo el nombre
-	ofn.nMaxFileTitle = sizeof(szFileTitle); //Poniendo el tam max del titulo
-
-	ofn.lpstrInitialDir = NULL; // Directorio inicial
-
-	ofn.lpstrFilter = "Todos los Archivos\0"; // Filtros
-	ofn.nFilterIndex = 1; // Filtro predeterminado
-
-	ofn.Flags = OFN_PATHMUSTEXIST | OFN_FILEMUSTEXIST; //Verifica si la ruta existe o el archivo existe
-
-	// Mostrar el cuadro de diálogo
+  // Estructura OPENFILENAME para configurar el diálogo
+  OPENFILENAME ofn;
+  char szFile[260] = {0};
+  char szFileTitle[260] = {0};
+  // Inicializar la estructura OPENFILENAME
+  ZeroMemory(&ofn, sizeof(ofn));
+  ofn.lStructSize = sizeof(ofn);
+  ofn.hwndOwner = NULL; // Ventana propietaria (NULL para consola)
+  ofn.lpstrFile = szFile; // Buffer para el archivo seleccionado
+  ofn.nMaxFile = sizeof(szFile); // Tamaño del buffer
+  ofn.lpstrFileTitle = szFileTitle; //Guarda el nombre ya sea con la ruta completa o solo el nombre
+  ofn.nMaxFileTitle = sizeof(szFileTitle); //Poniendo el tam max del titulo
+  ofn.lpstrInitialDir = NULL; // Directorio inicial
+  ofn.lpstrFilter = "Todos los Archivos\0"; // Filtros
+  ofn.nFilterIndex = 1; // Filtro predeterminado
+  ofn.Flags = OFN_PATHMUSTEXIST | OFN_FILEMUSTEXIST; //Verifica si la ruta existe o el archivo existe
+  // Mostrar el cuadro de diálogo
 	if (GetOpenFileName(&ofn))
-	{
-		char rutaSinNombre[260];
-		int i;
+  {
+		int i, j;
 
 		int tamRutaSinNombreArchivo = strlen(ofn.lpstrFile) - strlen(ofn.lpstrFileTitle);
 		int tamNombreArchivo = strlen(ofn.lpstrFileTitle);
@@ -70,10 +61,29 @@ int obtenerRuta(char ** rutaSinNombreArchivo, char ** nombreArchivo)
 			(*rutaSinNombreArchivo)[i] = ofn.lpstrFile[i];
 		(*rutaSinNombreArchivo)[i] = '\0';
 
-		for(i = 0; i <  strlen(ofn.lpstrFileTitle); i++)
-			(*nombreArchivo)[i] = ofn.lpstrFileTitle[i];
+		for(i = 0; i < tamNombreArchivo; i++)
+    {
+      if(ofn.lpstrFileTitle[i] == '.')
+        break;
+      (*nombreArchivo)[i] = ofn.lpstrFileTitle[i];
+    }
 		(*nombreArchivo)[i] = '\0';
+		int tamExtension = tamNombreArchivo - i;
 
+		*extension = malloc(sizeof(char) * (tamExtension));
+		if(*extension == NULL)
+			exit(-1);
+
+		printf("\n");
+
+		int x = 0;
+
+		for(j = i; j < tamNombreArchivo; j++)
+		{
+      (*extension)[x] = ofn.lpstrFileTitle[j];
+			x++;
+		}
+    (*extension)[j] = '\0';
 		return 1;
 	} else
 	{
